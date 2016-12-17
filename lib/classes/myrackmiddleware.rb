33170fc484c78e06be8676fe1cd72399e8d8b0fc
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-17 12:36:05 +0200

    Use middleware for check guess valid

---
+++ lib/classes/myrackmiddleware.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'Codebreaker_ML'
+
+class MyRackMiddleware
+  def initialize(appl)
+    @appl = appl
+  end
+
+  def call(env)
+    @request = Rack::Request.new(env)
+    case @request.path
+    when '/guess'
+      return @appl.call(env) if Codebreaker::GameController.validCode?(@request.params['guess'])
+      data = { status: Codebreaker::Game::TEXT[:incorrect] }
+      Rack::Response.new(data.to_json, 200, 'Content-Type' => 'application/json')
+    else
+      @appl.call(env)
+    end
+  end
+end
