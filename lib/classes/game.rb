6be33a20c7134148129b123018646f59c36ca8ec
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-17 14:52:29 +0200

    implement game process

--- lib/classes/game.rb
+++ lib/classes/game.rb
@@ -22,21 +22,27 @@ class Game
         response.redirect('/')
       end
     when '/restart'
-      @game = @request.session[:game] = Codebreaker::Game.new
+      @game = @request.session[:game] = nil
       Rack::Response.new do |response|
         response.redirect('/')
       end
     when '/hint'
       render_response(@game.hint)
     when '/guess'
-        answer = {}
-        answer['result'] = @game.guess(@request.params['guess'])
-        if @game.status.nil?
-          render_response(answer)
-        else
-          answer['game_over'] = @game.status
-          render_response(answer)
-        end
+      answer = {}
+      answer[:result] = @game.guess(@request.params['guess'])
+      if @game.status.nil?
+        render_response(answer)
+      else
+        answer[:status] = 'game_over'
+        answer[:text] = text(@game.status)
+        render_response(answer)
+      end
+    when '/show_history'
+      render_response(@game.show_history)
+    when '/save_history'
+      render_response(@game.save_history(@request.params['name']))
+      
     else Rack::Response.new('Not Found', 404)
     end
   end
@@ -50,8 +56,15 @@ class Game
     Rack::Response.new(data.to_json, 200, 'Content-Type' => 'application/json')
   end
 
-  def game
-    @game.instance_variable_get('@secret_code')
+  def index
+    answer = {}
+    if @game.nil?
+      answer[:status] = 'new_game'
+      answer[:text] =   'GAME RULES' # text(:rules)
+    else
+      answer[:status] = 'game_started'
+    end
+    answer.to_json
   end
 
   def text(message)
