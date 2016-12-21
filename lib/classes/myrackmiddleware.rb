eacff22d5668f0bc2e192dc42c5a8516af58357c
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-21 19:08:21 +0200

    Namming

--- lib/classes/myrackmiddleware.rb
+++ lib/classes/myrackmiddleware.rb
@@ -2,19 +2,19 @@ require 'json'
 require 'Codebreaker_ML'
 
 class MyRackMiddleware
-  def initialize(appl)
-    @appl = appl
+  def initialize(app)
+    @app = app
   end
 
   def call(env)
     @request = Rack::Request.new(env)
     case @request.path
     when '/guess'
-      return @appl.call(env) if Codebreaker::GameController.validCode?(@request.params['guess'])
+      return @app.call(env) if Codebreaker::GameController.validCode?(@request.params['guess'])
       data = { status: 'invalid_input' ,text: Codebreaker::Game::TEXT[:incorrect] }
       Rack::Response.new(data.to_json, 200, 'Content-Type' => 'application/json')
     else
-      @appl.call(env)
+      @app.call(env)
     end
   end
 end
