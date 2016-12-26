b2817e6c39d3cd6be54fb759f69bd33a4f145e6d
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-26 13:48:02 +0200

    add tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,55 +1,59 @@
 require 'spec_helper'
-require "rack/test"
+require 'rack/test'
 
 feature Game do
   scenario 'Open Main Page' do
     visit '/'
-    expect(page).to have_content('Rules')
+    expect(page).to have_content('RULES')
   end
 
-  # scenario 'Show Game Page' do
-  #   visit '/'
-  #   page.find("#start_game_btn").click
-  #   expect(page).to have_content('Guess')
-  # end
-
-  # scenario 'Return code 200 when path in known' do
-  #   visit '/start'
-  #   expect(status_code).to be(200)
-  # end  
+  scenario 'Return code 200 when path in known' do
+    visit '/start'
+    expect(status_code).to be(200)
+  end
 
   scenario 'Return code 404 when path in unknown' do
     visit '/unknown'
     expect(status_code).to be(404)
   end
 
-  # scenario 'Return error when invalid input' do
-  #   visit '/guess?guess=FAIL'
-  #   expect(page).to match(/invalid_input/)
-  # end  
+  scenario 'Return error when invalid Guess input' do
+    visit '/guess?guess=FAIL'
+    expect(page).to have_content(/invalid_input/)
+  end
 
-  # scenario 'Return result when input is correct' do
-  #   visit '/guess?guess=1234'
-  #   expect(page).to match(/result/)
-  # end  
+  scenario 'Return result when Guess input is correct' do
+    visit '/start'
+    visit '/guess?guess=1234'
+    expect(page).to have_content(/result/)
+  end
 
-  # scenario 'Return error when invalid input' do
-  #   visit '/restart'
-  #   expect(status_code).to be(200)
-  #   expect(page).to have_content('Rules')
-  # end  
+  scenario 'Game over when attempts have ended' do
+    visit '/start'
+    15.times { visit '/guess?guess=1111' }
+    expect(page).to have_content(/\"status\":\"game_over\"/)
+  end
 
-  # scenario '#hint' do
-  #   visit '/hint'
-  #   expect(status_code).to be(200)
-  #   expect(page).to match(/[1-6]/)
-  # end  
+  scenario 'Show game progress when game ended' do
+    visit '/start'
+    15.times { visit '/guess?guess=1111' }
+    expect(page).to have_content(/Secret code was: /)
+  end
 
-  # scenario '#show_history' do
-  #   visit '/show_history'
-  #   expect(status_code).to be(200)
-  #   expect(page).to match(/Secret code was: /)
-  # end
+  scenario 'Save game process' do
+    visit '/start'
+    15.times { visit '/guess?guess=1111' }
+    visit '/save_history?name=TEST'
+    expect(page).to have_content(/\"saved\"/)
+  end
+
+  scenario 'Show games history' do
+    visit '/start'
+    15.times { visit '/guess?guess=1111' }
+    visit '/save_history?name=TEST_NAME'
+    visit '/show_history'
+    expect(page).to have_content(/\"TEST_NAME\"/)
+  end
 end
 
 describe Game do
@@ -63,7 +67,7 @@ describe Game do
     last_request.env['rack.session']
   end
 
-  context 'Return code 404 when path in unknown' do
+  context 'Return code 404 when path in unknown', skip: false do
     let(:response) { post '/unknown' }
 
     it 'Return code 404' do
@@ -74,7 +78,7 @@ describe Game do
     end
   end
 
-  context 'Open Main Page' do
+  context 'Open Main Page', skip: false do
     let(:response) { post '/' }
 
     it 'Return code 200' do
@@ -83,16 +87,43 @@ describe Game do
     it 'body text: Start Game' do
       expect(response.body).to match(/Start Game/)
     end
-  end  
+  end
 
-  # context 'Start Game' do
-  #   let(:response) { post '/start' }
+  context 'Start Game', skip: false do
+    let(:response) { post '/start' }
 
-  #   it 'Return code 200' do
-  #     expect(response.status).to eql(200)
-  #   end
-  #   it 'body text: Guess' do
-  #     expect(response.body).to match(/Guess/)
-  #   end
-  # end
+    it 'Return code 200' do
+      expect(response.status).to eql(200)
+    end
+    it 'body text: Guess' do
+      expect(response.body).to match(/status\":\"game_started/)
+    end
+  end
+
+  context 'Show Hint', skip: false do
+    let(:response) { post '/hint' }
+    let(:start) { post '/start' }
+    it 'body text: Guess' do
+      start
+      expect(response.body).to match(/[1-6]/)
+    end
+  end
+
+  context 'Return result when input is correct', skip: false do
+    let(:response) { post '/guess?guess=1111' }
+    let(:start) { post '/start' }
+    it 'body text: Guess' do
+      start
+      expect(response.body).to match(/"result\":/)
+    end
+  end
+
+  context 'Return error when invalid input', skip: false do
+    let(:response) { post '/guess?guess=invalid_input' }
+    let(:start) { post '/start' }
+    it 'body text: Guess' do
+      start
+      expect(response.body).to match(/invalid_input/)
+    end
+  end
 end
