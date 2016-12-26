d9356a922d073f14f04f5dd842086c87be8b6d8b
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-12-26 18:38:27 +0200

    Edit error message

--- lib/classes/myrackmiddleware.rb
+++ lib/classes/myrackmiddleware.rb
@@ -8,7 +8,7 @@ class MyRackMiddleware
     case @request.path
     when '/guess'
       return @app.call(env) if Codebreaker::GameController.valid_code?(@request.params['guess'])
-      data = { status: 'invalid_input', text: 'invalid input' }
+      data = { status: 'invalid_input', text: 'Invalid entry valid values, digits 1-6' }
       Rack::Response.new(data.to_json, 200, 'Content-Type' => 'application/json')
     else
       @app.call(env)
